1f6b60b3f841e3ea753acbf9ebe2972f8b2e5453
EkaFord <138514574+EkaFord@users.noreply.github.com>
2023-10-31 15:07:06 -0400

    styled components 1

--- skypro-music/src/index.js
+++ skypro-music/src/index.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import "./components/AudioPlayer/AudioPlayer.css";
-import "./components/NavMenu/NavMenu.css";
-import "./components/BurgerMenu/BurgerMenu.css";
-import "./components/TrackList/TrackList.css";
-import "./components/TracksBlock/TracksBlock.css";
-import "./components/Track/Track.css";
-import "./components/SideBar/SideBar.css";
-import "./components/TrackSkeleton/TrackSkeleton.css";
-import "./components/PlayLists/PlayLists.css";
-import "./components/PlayList/PlayList.css";
-import "./components/FilterButtons/FilterButtons.css";
-import "./components/AudioPlayerLoad/AudioPlayerLoad.css";
 
 import App from './App';
-import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -23,7 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
