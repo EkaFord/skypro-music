813ee4ff4d8c6ac206c6290389064e07d80288fa
EkaFord <138514574+EkaFord@users.noreply.github.com>
2023-11-13 12:17:59 -0500

    api and tracks

--- skypro-music/src/pages/main/components/AudioPlayer/AudioPlayer.jsx
+++ skypro-music/src/pages/main/components/AudioPlayer/AudioPlayer.jsx
@@ -1,101 +1,191 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AudioPlayerLoad from "../AudioPlayerLoad/AudioPlayerLoad";
-import { useContext } from 'react';
+import { useContext, useState, useRef } from 'react';
 import LoadingContext from '../context';
 import * as S from "./AudioPlayerStyles"
+// import ProgressBar from "../ProgresState";
 
 
 const AudioPlayer = () => {
 
-  const { loading, setLoading } = useContext(LoadingContext)
+  const { loading, currentTrack } = useContext(LoadingContext)
+  const [isPlaying, setPlaying] = useState(false);
+  const [isRepeat, setIsRepeat] = useState(false);
+
+
+  const ref = useRef(0);
+
+  const handleStart = () => {
+    ref.current.play();
+  };
+
+  useEffect(handleStart, [currentTrack])
+
+
+  const handleStop = () => {
+    ref.current.pause();
+  };
+
+  function sToStr(s) {
+    let m = Math.trunc(s / 60) + ''
+    s = (s % 60) + ''
+    let sec = Math.floor(s.padStart(2, 0))
+    if (sec < 10) {
+      sec = `0` + Math.floor(s.padStart(2, 0))
+    }
+    return m.padStart(2, 0) + ':' + `${sec}`
+  }
+
+  const [currentTime, setCurrentTime] = useState(0);
+  // const [currentTime, setCurrentTime] = useState(70);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(sToStr(ref.current.currentTime))
+    }, 1000);
+    return () => clearTimeout(interval);
+  }, [currentTrack])
+
+  const handleRepeat = () => {
+    ref.current.loop = !isRepeat;
+    setIsRepeat(!isRepeat)
+  };
+
+  const awaitImplementation = () => {
+    alert('Not available yet');
+  };
 
   return (
-    <S.Bar>
-      <S.BarContent>
-        <S.BarPlayerProgress></S.BarPlayerProgress>
-        <S.BarPlayerBlock>
-          <S.BarPlayer>
-            <S.PlayerControls>
-              <S.PlayerBtnPrev>
-                <S.PlayerBtnPrevSvg alt="prev">
-                  <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
-                </S.PlayerBtnPrevSvg>
-              </S.PlayerBtnPrev>
-              <S.PlayerBtnPlay>
-                <S.PlayerBtnPlaySvg as="svg" alt="play">
-                  <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
-                </S.PlayerBtnPlaySvg>
-              </S.PlayerBtnPlay>
-              <S.PlayerBtnNext>
-                <S.PlayerBtnNextSvg alt="next">
-                  <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
-                </S.PlayerBtnNextSvg>
-              </S.PlayerBtnNext>
-              <S.PlayerBtnRepeat>
-                <S.PlayerBtnRepeatSvg alt="repeat">
-                  <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
-                </S.PlayerBtnRepeatSvg>
-              </S.PlayerBtnRepeat>
-              <S.PlayerBtnShuffle>
-                <S.PlayerBtnShuffleSvg alt="shuffle">
-                  <use xlinkHref="img/icon/sprite.svg#icon-shuffle"></use>
-                </S.PlayerBtnShuffleSvg>
-              </S.PlayerBtnShuffle>
-            </S.PlayerControls>
+    <>
+      <audio
+        ref={ref}
+        controls="controls"
+        // {isRepeat ? loop = "loop" : null}
+        src={currentTrack.track_file}
+        onPlay={() => setPlaying(true)}
+        onPause={() => setPlaying(false)}
+      ></audio>
+      <S.Bar>
+        <S.BarContent>
+          <S.TimeCode>{currentTime} / {sToStr(ref.current.duration)}</S.TimeCode>
+          {/* <S.BarPlayerProgress></S.BarPlayerProgress> */}
+          <S.StyledProgressInput
+            type="range"
+            min={0}
+            max={ref.current.duration}
+            value={currentTime}
+            step={0.01}
+            onChange={(a) => {
+              ref.current.currentTime = a.target.value;
+            }}
+            // onChange={(event) => setCurrentTime(event.target.value)}
+            color="#b672ff"
+          />
+          <S.BarPlayerBlock>
+            <S.BarPlayer>
+              <S.PlayerControls>
+                <S.PlayerBtnPrev>
+                  <S.PlayerBtnPrevSvg onClick={awaitImplementation} alt="prev">
+                    <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
+                  </S.PlayerBtnPrevSvg>
+                </S.PlayerBtnPrev>
+                {isPlaying ?
+                  <S.PlayerBtnPlay onClick={handleStop}>
+                    <S.PlayerBtnPlaySvg as="svg" alt="play">
+                      <svg width="15" height="19" viewBox="0 0 15 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="5" height="19" fill="#D9D9D9" />
+                        <rect x="10" width="5" height="19" fill="#D9D9D9" />
+                      </svg>
+                    </S.PlayerBtnPlaySvg>
+                  </S.PlayerBtnPlay>
+                  :
+                  <S.PlayerBtnPlay onClick={handleStart}>
+                    <S.PlayerBtnPlaySvg as="svg" alt="play">
+                      <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
+                    </S.PlayerBtnPlaySvg>
+                  </S.PlayerBtnPlay>
 
-            <S.PlayerTrackPlay>
+                }
+                <S.PlayerBtnNext>
+                  <S.PlayerBtnNextSvg onClick={awaitImplementation} alt="next">
+                    <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
+                  </S.PlayerBtnNextSvg>
+                </S.PlayerBtnNext>
+                {isRepeat ?
+                  <S.PlayerBtnRepeat onClick={handleRepeat}>
+                    <S.PlayerBtnRepeatActiveSvg alt="repeat">
+                      <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
+                    </S.PlayerBtnRepeatActiveSvg>
+                  </S.PlayerBtnRepeat>
 
-              {loading ? <AudioPlayerLoad /> :
-                <S.TrackPlayContain>
-                  <S.TrackPlayImage>
-                    <S.TrackPlaySvg alt="music">
-                      <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
-                    </S.TrackPlaySvg>
-                  </S.TrackPlayImage>
-                  <S.TrackPlayAuthor>
-                    <S.TrackPlayAuthorLink xlinkHref="http://">
-                      Ты та...
-                    </S.TrackPlayAuthorLink>
-                  </S.TrackPlayAuthor>
-                  <S.TrackPlayAlbum>
-                    <S.TrackPlayAlbumLink xlinkHref="http://">
-                      Баста
-                    </S.TrackPlayAlbumLink>
-                  </S.TrackPlayAlbum>
-                </S.TrackPlayContain>}
+                  :
+                  <S.PlayerBtnRepeat onClick={handleRepeat}>
+                    <S.PlayerBtnRepeatSvg alt="repeat">
+                      <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
+                    </S.PlayerBtnRepeatSvg>
+                  </S.PlayerBtnRepeat>
+                }
+                <S.PlayerBtnShuffle>
+                  <S.PlayerBtnShuffleSvg onClick={awaitImplementation} alt="shuffle">
+                    <use xlinkHref="img/icon/sprite.svg#icon-shuffle"></use>
+                  </S.PlayerBtnShuffleSvg>
+                </S.PlayerBtnShuffle>
+              </S.PlayerControls>
 
-              <S.TrackPlayLikeDis>
-                <S.TrackPlayLike>
-                  <S.TrackPlayLikeSvg alt="like">
-                    <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
-                  </S.TrackPlayLikeSvg>
-                </S.TrackPlayLike>
-                <S.TrackPlayDislike>
-                  <S.TrackPlayDislikeSvg alt="dislike">
-                    <use xlinkHref="img/icon/sprite.svg#icon-dislike"></use>
-                  </S.TrackPlayDislikeSvg>
-                </S.TrackPlayDislike>
-              </S.TrackPlayLikeDis>
-            </S.PlayerTrackPlay>
-          </S.BarPlayer>
-          <S.BarVolumeBlock>
-            <S.VolumeContent>
-              <S.VolumeImage>
-                <S.VolumeSvg alt="volume">
-                  <use xlinkHref="img/icon/sprite.svg#icon-volume"></use>
-                </S.VolumeSvg>
-              </S.VolumeImage>
-              <S.VolumeProgress>
-                <S.VolumeProgressLine as="input"
-                  type="range"
-                  name="range"
-                />
-              </S.VolumeProgress>
-            </S.VolumeContent>
-          </S.BarVolumeBlock>
-        </S.BarPlayerBlock>
-      </S.BarContent>
-    </S.Bar >
+              <S.PlayerTrackPlay>
+
+                {loading ? <AudioPlayerLoad /> :
+                  <S.TrackPlayContain>
+                    <S.TrackPlayImage>
+                      <S.TrackPlaySvg alt="music">
+                        <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
+                      </S.TrackPlaySvg>
+                    </S.TrackPlayImage>
+                    <S.TrackPlayAuthor>
+                      <S.TrackPlayAuthorLink xlinkHref="http://">
+                        {currentTrack.name}
+                      </S.TrackPlayAuthorLink>
+                    </S.TrackPlayAuthor>
+                    <S.TrackPlayAlbum>
+                      <S.TrackPlayAlbumLink xlinkHref="http://">
+                        {currentTrack.author}
+                      </S.TrackPlayAlbumLink>
+                    </S.TrackPlayAlbum>
+                  </S.TrackPlayContain>}
+
+                <S.TrackPlayLikeDis>
+                  <S.TrackPlayLike onClick={awaitImplementation}>
+                    <S.TrackPlayLikeSvg alt="like">
+                      <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
+                    </S.TrackPlayLikeSvg>
+                  </S.TrackPlayLike>
+                  <S.TrackPlayDislike onClick={awaitImplementation}>
+                    <S.TrackPlayDislikeSvg alt="dislike">
+                      <use xlinkHref="img/icon/sprite.svg#icon-dislike"></use>
+                    </S.TrackPlayDislikeSvg>
+                  </S.TrackPlayDislike>
+                </S.TrackPlayLikeDis>
+              </S.PlayerTrackPlay>
+            </S.BarPlayer>
+            <S.BarVolumeBlock>
+              <S.VolumeContent>
+                <S.VolumeImage>
+                  <S.VolumeSvg alt="volume">
+                    <use xlinkHref="img/icon/sprite.svg#icon-volume"></use>
+                  </S.VolumeSvg>
+                </S.VolumeImage>
+                <S.VolumeProgress>
+                  <S.VolumeProgressLine as="input"
+                    type="range"
+                    name="range"
+                  />
+                </S.VolumeProgress>
+              </S.VolumeContent>
+            </S.BarVolumeBlock>
+          </S.BarPlayerBlock>
+        </S.BarContent>
+      </S.Bar >
+    </>
   );
 };
 
--- skypro-music/src/pages/main/components/AudioPlayer/AudioPlayerStyles.js
+++ skypro-music/src/pages/main/components/AudioPlayer/AudioPlayerStyles.js
@@ -1,19 +1,14 @@
 import styled, { keyframes } from 'styled-components'
 
 export const Bar = styled.div`
-  position: absolute;
+  /* position: absolute; */
   bottom: 0;
   left: 0;
   width: 100%;
   background: rgba(28, 28, 28, 0.5);
 `
 export const BarContent = styled.div`
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: column;
   flex-direction: column;
 `
 
@@ -24,60 +19,31 @@ export const BarPlayerProgress = styled.div`
 `
 export const BarPlayerBlock = styled.div`
   height: 73px;
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-orient: horizontal;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: row;
   flex-direction: row;
-  -webkit-box-pack: justify;
-  -ms-flex-pack: justify;
   justify-content: space-between;
 `
 export const BarPlayer = styled.div`
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-orient: horizontal;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: row;
   flex-direction: row;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
   align-items: center;
-  -webkit-box-pack: start;
-  -ms-flex-pack: start;
   justify-content: flex-start;
 `
 export const BarVolumeBlock = styled.div`
   width: auto;
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
   align-items: center;
   padding: 0 92px 0 0;
 `
 export const PlayerControls = styled.div`
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-orient: horizontal;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: row;
   flex-direction: row;
   padding: 0 27px 0 31px;
 `
 
 export const StylesPrevPlayNextRepeatShuffle = styled.div`
   padding: 5px;
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
   align-items: center;
 `
 
@@ -113,6 +79,8 @@ export const PlayerBtnRepeatSvg = styled.svg`
   stroke: #696969;
 `
 
+
+
 export const PlayerBtnShuffleSvg = styled.svg`
   width: 19px;
   height: 12px;
@@ -121,24 +89,15 @@ export const PlayerBtnShuffleSvg = styled.svg`
 `
 
 export const PlayerTrackPlay = styled.div`
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-orient: horizontal;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: row;
   flex-direction: row;
 `
 
 export const TrackPlayContain = styled.div`
   width: auto;
-  display: -ms-grid;
   display: grid;
-  -ms-grid-columns: auto 1fr;
   grid-template-columns: auto 1fr;
   grid-template-areas: "image author" "image album";
-  -webkit-box-align: center;
-  -ms-flex-align: center;
   align-items: center;
 `
 
@@ -146,19 +105,10 @@ export const TrackPlayImage = styled.div`
   width: 51px;
   height: 51px;
   background-color: #313131;
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
   align-items: center;
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
   justify-content: center;
   margin-right: 12px;
-  -ms-grid-row: 1;
-  -ms-grid-row-span: 2;
-  -ms-grid-column: 1;
   grid-area: image;
 `
 
@@ -169,10 +119,10 @@ export const TrackPlaySvg = styled.svg`
   stroke: #4e4e4e;
 `
 export const TrackPlayAuthor = styled.div`
-  -ms-grid-row: 1;
-  -ms-grid-column: 2;
   grid-area: author;
   min-width: 49px;
+  text-overflow: ellipsis;
+  overflow: hidden;
 `
 export const TrackPlayAuthorLink = styled.a`
   font-style: normal;
@@ -184,10 +134,10 @@ export const TrackPlayAuthorLink = styled.a`
 `
 
 export const TrackPlayAlbum = styled.div`
-  -ms-grid-row: 2;
-  -ms-grid-column: 2;
   grid-area: album;
   min-width: 49px;
+  text-overflow: ellipsis;
+  overflow: hidden;
 `
 
 export const TrackPlayAlbumLink = styled.a`
@@ -196,25 +146,19 @@ export const TrackPlayAlbumLink = styled.a`
   font-size: 13px;
   line-height: 24px;
   color: #ffffff;
+  white-space: nowrap;
 `
 
 export const TrackPlayLikeDis = styled.div`
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-orient: horizontal;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: row;
   flex-direction: row;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
   align-items: center;
   margin-left: 26%;
 `
 
 
 
-export const LikeDislikeSvg  = styled.svg`
+export const LikeDislikeSvg = styled.svg`
   &&:active {
     fill: #696969;
     stroke: #ffffff;
@@ -235,22 +179,13 @@ export const TrackPlayDislikeSvg = styled(LikeDislikeSvg)`
   stroke: #696969;
 `
 export const VolumeContent = styled.div`
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-orient: horizontal;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: row;
   flex-direction: row;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
   align-items: center;
-  -webkit-box-pack: end;
-  -ms-flex-pack: end;
   justify-content: end;
 `
 
-export const VolumeImage  = styled.div`
+export const VolumeImage = styled.div`
   width: 13px;
   height: 18px;
   margin-right: 17px;
@@ -260,11 +195,11 @@ export const VolumeSvg = styled.svg`
   height: 18px;
   fill: transparent;
 `
-export const Btn  = styled.div`
+export const Btn = styled.div`
   cursor: pointer;
 `
 
-export const BtnIcon  = styled(Btn)`
+export const BtnIcon = styled(Btn)`
   &:hover svg {
     fill: transparent;
     stroke: #acacac;
@@ -274,14 +209,14 @@ export const BtnIcon  = styled(Btn)`
     stroke: #ffffff;
   }
 `
-export const TrackPlayLike  = styled(BtnIcon)`
+export const TrackPlayLike = styled(BtnIcon)`
   padding: 5px;
 `
-export const TrackPlayDislike  = styled(TrackPlayLike)`
+export const TrackPlayDislike = styled(TrackPlayLike)`
   margin-left: 28.5px;
 `
 
-export const PlayerBtnsSvg  = styled(BtnIcon)`
+export const PlayerBtnsSvg = styled(BtnIcon)`
   padding: 5px;
   display: flex;
   align-items: center;
@@ -295,13 +230,13 @@ export const PlayerBtnRepeat = styled(PlayerBtnsSvg)`
   margin-right: 24px;
 `
 
-export const PlayerBtnShuffle  = styled(PlayerBtnsSvg)`
+export const PlayerBtnShuffle = styled(PlayerBtnsSvg)`
 `
 
-export const VolumeProgress  = styled(Btn) `
+export const VolumeProgress = styled(Btn)`
   width: 109px;
 `
-export const VolumeProgressLine  = styled(Btn) `
+export const VolumeProgressLine = styled(Btn)`
   width: 109px;
 `
 
@@ -321,8 +256,79 @@ const SkeletonAnim = keyframes`
 export const TrackPlayImageSkeleton = styled(TrackPlayImage)`
   animation: ${SkeletonAnim} 3s linear infinite both;
 `
-export const TrackPlaySkeleton  = styled.div`
+export const TrackPlaySkeleton = styled.div`
   width: 59px;
   height: 15px;
   animation: ${SkeletonAnim} 3s linear infinite both;
 `
+
+export const PlayerBtnRepeatActiveSvg = styled.svg`
+  width: 18px;
+  height: 12px;
+  fill: transparent;
+  stroke: #ffffff;
+  &:hover  {
+    fill: transparent;
+    stroke: #ffffff;
+  }
+`
+
+export const TimeCode = styled.div`
+    color: #696969;
+    text-align: end;
+    margin: 10px 50px 10px;
+`
+
+export const StyledProgressInput = styled.input`
+  --progress-height: 8px;
+  --progress-color: #b672ff;
+  --progress-bg-color: #2e2e2e;
+
+  margin: 0;
+  width: 100%;
+  height: var(--progress-height);
+  -webkit-appearance: none;
+  cursor: pointer;
+  background: transparent;
+  position: relative;
+  overflow: hidden;
+
+  &::-webkit-slider-runnable-track {
+    position: relative;
+    height: var(--progress-height);
+    background: var(--progress-bg-color);
+  }
+  &::-webkit-slider-thumb {
+    --thumb-height: 1px;
+    --thumb-width: 1px;
+    position: relative;
+    -webkit-appearance: none;
+    width: var(--thumb-width, var(--thumb-height));
+    box-shadow: calc(-100vmax - var(--thumb-width, var(--thumb-height))) 0 0
+      100vmax var(--progress-color);
+  }
+
+  &::-webkit-slider-runnable-track {
+    background: var(--progress-bg-color);
+  }
+
+  /* FF */
+  &::-moz-range-track {
+    width: 100%;
+    height: var(--progress-height);
+    background: var(--progress-bg-color);
+    border: none;
+    border-radius: 0px;
+  }
+  &::-moz-range-thumb {
+    border: none;
+    height: 25px;
+    width: 25px;
+    border-radius: 50%;
+    background: transparent;
+  }
+  &::-moz-range-progress {
+    background-color: var(--progress-color);
+    height: var(--progress-height);
+  }
+`;
