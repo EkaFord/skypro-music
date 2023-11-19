b7e8566109656c7750fd031724ca9e483638a55d
EkaFord <138514574+EkaFord@users.noreply.github.com>
2023-11-19 09:22:21 -0500

    better track

--- skypro-music/src/pages/main/components/Track/Track.jsx
+++ skypro-music/src/pages/main/components/Track/Track.jsx
@@ -1,67 +1,59 @@
 import React from "react";
-import * as S from "./TrackStyles"
-import { getTodos } from "../../../../api";
-import { useEffect, useState } from "react";
-import TrackSkeleton from "../TrackSkeleton/TrackSkeleton";
+import * as S from "./TrackStyles";
 import { useContext } from 'react';
 import LoadingContext from '../context';
-
-
+import TrackSkeleton from "../TrackSkeleton/TrackSkeleton";
 
 const Track = () => {
+  const { tracks, loadings, addTracksError, setCurrentTrack } = useContext(LoadingContext);
 
-  const { tracks, loadings, addTracksError, currentTrack, setCurrentTrack } = useContext(LoadingContext)
-
-  function sToStr(s) {
-    let m = Math.trunc(s / 60) + ''
-    s = (s % 60) + ''
-    return m.padStart(2, 0) + ':' + s.padStart(2, 0)
-  }
+  const secondsToTime = (seconds) => {
+    const minutes = Math.trunc(seconds / 60);
+    const remainingSeconds = (seconds % 60).toString().padStart(2, '0');
+    return `${minutes}:${remainingSeconds}`;
+  };
 
   return (
     <>
-      {loadings ? <TrackSkeleton /> : null}
-      {addTracksError ? <p>Не удалось загрузить плейлист, попробуйте позже</p> : null}
-      {tracks.map((track) => {
-        return (
-          <S.PlaylistItem key={track.id}>
-            <S.PlaylistTrack>
-              <S.TrackTitle onClick={() => setCurrentTrack(track)}>
-                <S.TrackTitleImage>
-                  <S.TrackTitleSvg alt="music">
-                    <use xlinkHref="img/icon/sprite.svg#icon-note" />
-                  </S.TrackTitleSvg>
-                </S.TrackTitleImage>
-                <div>
-                  <S.TrackTitleLink href="#">
-                    {track.name} <S.TrackTitleSpan />
-                  </S.TrackTitleLink>
-                </div>
-              </S.TrackTitle>
-              <S.TrackAuthor>
-                <S.TrackAuthorLink href="http://">
-                  {track.author}
-                </S.TrackAuthorLink>
-              </S.TrackAuthor>
-              <S.TrackAlbum>
-                <S.TrackAlbumLink href="http://">
-                  {track.album}
-                </S.TrackAlbumLink>
-              </S.TrackAlbum>
+      {loadings && <TrackSkeleton />}
+      {addTracksError && <p>Failed to load playlist. Please try again later.</p>}
+      {tracks.map((track) => (
+        <S.PlaylistItem key={track.id}>
+          <S.PlaylistTrack>
+            <S.TrackTitle onClick={() => setCurrentTrack(track)}>
+              <S.TrackTitleImage>
+                <S.TrackTitleSvg alt="music">
+                  <use xlinkHref="img/icon/sprite.svg#icon-note" />
+                </S.TrackTitleSvg>
+              </S.TrackTitleImage>
               <div>
-                <S.TrackTimeSvg alt="time">
-                  <use xlinkHref="img/icon/sprite.svg#icon-like" />
-                </S.TrackTimeSvg>
-                <S.TrackTimeText>{sToStr(track.duration_in_seconds)}
-                </S.TrackTimeText>
+                <S.TrackTitleLink href="#">
+                  {track.name} <S.TrackTitleSpan />
+                </S.TrackTitleLink>
               </div>
-            </S.PlaylistTrack>
-          </S.PlaylistItem>
-        )
-      })}
-
+            </S.TrackTitle>
+            <S.TrackAuthor>
+              <S.TrackAuthorLink href="http://">
+                {track.author}
+              </S.TrackAuthorLink>
+            </S.TrackAuthor>
+            <S.TrackAlbum>
+              <S.TrackAlbumLink href="http://">
+                {track.album}
+              </S.TrackAlbumLink>
+            </S.TrackAlbum>
+            <div>
+              <S.TrackTimeSvg alt="time">
+                <use xlinkHref="img/icon/sprite.svg#icon-like" />
+              </S.TrackTimeSvg>
+              <S.TrackTimeText>{secondsToTime(track.duration_in_seconds)}
+              </S.TrackTimeText>
+            </div>
+          </S.PlaylistTrack>
+        </S.PlaylistItem>
+      ))}
     </>
   );
 };
 
-export default Track
+export default Track;
